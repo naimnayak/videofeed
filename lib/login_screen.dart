80e072d5b3378908109ac422294e2f2086841847
naimnayak <134128301+naimnayak@users.noreply.github.com>
2024-06-10 22:14:40 +0530

    Update login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'main.dart'; // Import FirebaseAuthService and UserProfile
-import 'video_feed_screen.dart';
-import 'profile_screen.dart';
+import '../main.dart'; // Import FirebaseAuthService and UserProfile
 import 'user_profile.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -19,7 +16,9 @@ class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+
   bool _isLoading = false;
+  bool _isPasswordObscured = true;
 
   @override
   void dispose() {
@@ -28,77 +27,150 @@ class _LoginScreenState extends State<LoginScreen> {
     super.dispose();
   }
 
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isPasswordObscured = !_isPasswordObscured;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      backgroundColor: Colors.grey[300],
       body: Form(
         key: _formKey,
-        child: Padding(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              const SizedBox(height: 100), // Add some space at the top
+              const Icon(Icons.lock, size: 100,),
+              Text('Welcome back!',
+                style: TextStyle(color: Colors.grey[700],
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+
+              const SizedBox(height: 58,),
+
               TextFormField(
                 controller: _emailController,
-                decoration: InputDecoration(labelText: 'Email'),
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400)
+                  ),
+                  fillColor: Colors.grey.shade200,
+                  filled: true,
+                ),
                 validator: (value) => value!.isEmpty ? 'Please enter email' : null,
               ),
-              SizedBox(height: 20),
+
+              SizedBox(height: 20,),
+
               TextFormField(
                 controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400)
+                  ),
+                  fillColor: Colors.grey.shade200,
+                  filled: true,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordObscured ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: _togglePasswordVisibility,
+                  ),
+                ),
+                obscureText: _isPasswordObscured,
                 validator: (value) => value!.isEmpty ? 'Please enter password' : null,
               ),
-              SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: _isLoading
-                    ? null
-                    : () async {
-                  if (_formKey.currentState!.validate()) {
-                    setState(() {
-                      _isLoading = true;
-                    });
+              const SizedBox(height: 20,),
 
-                    UserCredential? user =
-                    await _authService.signInWithEmailAndPassword(
-                      _emailController.text,
-                      _passwordController.text,
-                    );
+              Text('Forgot Password?',
+                style: TextStyle(color: Colors.grey[600]),),
 
-                    setState(() {
-                      _isLoading = false;
-                    });
+              const SizedBox(height: 25,),
 
-                    if (user != null) {
-                      // Fetch or create user profile
-                      UserProfile? userProfile = await _authService.getUserProfile(user.user!.uid);
-                      if (userProfile == null) {
-                        userProfile = UserProfile(
-                          uid: user.user!.uid,
-                          email: _emailController.text,
-                          username: 'new_user', // Default username
+              //sign
+
+              SizedBox(height: 10),
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: _isLoading
+                      ? null
+                      : () async {
+                    if (_formKey.currentState!.validate()) {
+                      setState(() {
+                        _isLoading = true;
+                      });
+
+                      try {
+                        UserCredential? user = await _authService.signInWithEmailAndPassword(
+                          _emailController.text,
+                          _passwordController.text,
                         );
-                        await _authService.createOrUpdateUserProfile(userProfile);
-                      }
-                      SharedPreferences prefs = await SharedPreferences.getInstance();
-                      await prefs.setBool('isLoggedIn', true);
 
-                      // Navigate to VideoFeedScreen
-                      Navigator.pushReplacementNamed(context, '/feed');
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                            content: Text('Invalid email or password')),
-                      );
+                        if (user != null) {
+                          UserProfile? userProfile = await _authService.getUserProfile(user.user!.uid);
+                          if (userProfile == null) {
+                            userProfile = UserProfile(
+                              uid: user.user!.uid,
+                              email: _emailController.text,
+                              username: 'new_user', // Default username
+                            );
+                            await _authService.createOrUpdateUserProfile(userProfile);
+                          }
+                          SharedPreferences prefs = await SharedPreferences.getInstance();
+                          await prefs.setBool('isLoggedIn', true);
+
+                          Navigator.pushReplacementNamed(context, '/feed');
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Invalid email or password')),
+                          );
+                        }
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Error: ${e.toString()}')),
+                        );
+                      } finally {
+                        setState(() {
+                          _isLoading = false;
+                        });
+                      }
                     }
-                  }
-                },
-                child: _isLoading
-                    ? CircularProgressIndicator()
-                    : Text('Login'),
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: _isLoading
+                      ? CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  )
+                      : Text('Login',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                    ),),
+                ),
               ),
+              const SizedBox(height: 20), // Add some space at the bottom
             ],
           ),
         ),
